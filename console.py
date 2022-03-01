81ff943119e234a20c9bded1ead3ffa6021710f8
EricAltez <ericaltez1111@gmail.com>
2022-03-01 12:09:10 -0800

    add unfinished updated console module

--- console.py
+++ console.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
 """
 Console module
+Entry point
+Uses python cmd
 """
 import cmd
+from models.base_model import BaseModel
+import json
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbtn) '
+
+    def do_create(self, command)
+        """
+        Create a class instance
+        """
+        if (!command):
+            print('** class name missing **')
+            return
+        new_obj.save()
+        print(new_obj.id)
+
+    def do_show(self, command):
+        """
+        Prints the string representation of a class instance nased on id
+        Usage: show <class_name> <id>
+        """
+        if (!command):
+            print('** class name missing **')
+            return
 
     def do_quit(self, command):
         """
