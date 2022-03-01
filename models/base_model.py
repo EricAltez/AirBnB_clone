a63fbddf1455f519292f445d5867051e5aacc430
flipeprez <flipeprez@gmail.com>
2022-03-01 06:28:33 -0800

    fix indentation

--- models/base_model.py
+++ models/base_model.py
@@ -16,22 +16,28 @@ class BaseModel:
             self.updated_at = datetime.now()
         else:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                                    "%Y-%m-%d:%H:%M:%S.%f")
+                                                     "%Y-%m-%d:%H:%M:%S.%f")
             kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                                    "%Y-%m-%d:%H:%M:%S.%f")
+                                                     "%Y-%m-%d:%H:%M:%S.%f")
 
     def __str__(self):
         '''string representation of basemodel'''
-        return(f"[{}] ({}) {}"(self.__class__.__name__, self.id, self.__dict__))
+        return(f"[{}] ({}) {}"(self.__class__.__name__,
+                               self.id, self.__dict__))
 
     def save(self):
         '''update the public instance attributte'''
         self.update_at = datetime.now()
 
     def to_dict(self):
-        '''returns a dictionary containing all keys/values of __dict__ instance'''
+        '''
+        returns a dictionary containing all
+        keys/values of __dict__ instance
+        '''
         cpy_dic = dict(self.__dict__)
         cpy_dic['__class__'] = self.__class__.__name__
-        cpy_dic['updated_at'] = self.updated_at.srtftime("%Y-%m-%d %H : %M : %S.%f")
-        cpy_dic['created_at'] = self.created_at.strftime("%Y-%m-%d %H : %M : %S.%f")
+        cpy_dic['updated_at'] = self.updated_at
+        .srtftime("%Y-%m-%d%H:%M :%S.%f")
+        cpy_dic['created_at'] = self.created_at
+        .strftime("%Y-%m-%d %H:%M:%S.%f")
         return(cpy_dic)
