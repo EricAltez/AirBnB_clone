9843b09982b97a15a45572960bf8a2e2d9c8cb26
flipeprez <flipeprez@gmail.com>
2022-03-02 04:59:23 -0800

    adding setattr

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""
+'''
 Base model module
 
-"""
+'''
 import uuid
-
+from datetime import datetime
 
 class BaseModel:
     '''class defines all commons attributes/methods for other classes'''
     def __init__(self, *args, **kwargs):
         '''function to inicialize all instances atrributes'''
-        if (kwargs == 0):
+        if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+            for key in kwargs:
+                kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
                                                      "%Y-%m-%d:%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
                                                      "%Y-%m-%d:%H:%M:%S.%f")
+                for key, val in kwargs.items():
+                    if "__class__" not in key:
+                        setattr(self, key, val)
 
     def __str__(self):
         '''string representation of basemodel'''
-        return(f"[{}] ({}) {}"(self.__class__.__name__,
-                               self.id, self.__dict__))
+        return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         '''update the public instance attributte'''
@@ -36,8 +39,6 @@ class BaseModel:
         '''
         cpy_dic = dict(self.__dict__)
         cpy_dic['__class__'] = self.__class__.__name__
-        cpy_dic['updated_at'] = self.updated_at
-        .srtftime("%Y-%m-%d%H:%M :%S.%f")
-        cpy_dic['created_at'] = self.created_at
-        .strftime("%Y-%m-%d %H:%M:%S.%f")
+        cpy_dic['updated_at'] = self.updated_at.srtftime("%Y-%m-%d%H:%M:%S.%f")
+        cpy_dic['created_at'] = self.created_at.strftime("%Y-%m-%d%H:%M:%S.%f")
         return(cpy_dic)
