eed92979759358e4341c1b1387f8e52d8630d599
EricAltez <ericaltez1111@gmail.com>
2022-03-02 16:23:37 -0800

    mod 1 byte on models/engine/__init__

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,7 +32,7 @@ class FileStorage:
             with open(FileStorage.__file_path) as wd:
                 FileStorage.__object = json.load(wd)
                 ax = json.load(wd)
-                    for key, val in ax.item():
-                        val = BaseModel(**val)
+                for key, val in ax.item():
+                    val = BaseModel(**val)
                     self.__object = ax
 
