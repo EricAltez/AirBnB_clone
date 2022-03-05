c3b2fabfb57d30c5d6c8c1598880c9d02a6cdf93
flipeprez <flipeprez@gmail.com>
2022-03-04 23:09:28 -0800

    first user and more classes

---
+++ models/Amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+write Amenity that inherits
+from BaseModel
+'''
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    ''' definition instance '''
+    name = ''
