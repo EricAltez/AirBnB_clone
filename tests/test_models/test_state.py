4141c442e25a17990535a9e56838ed5372b149b9
flipeprez <flipeprez@gmail.com>
2022-03-06 18:16:15 -0800

    tring to fix test

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-'''test state'''
-
+"""Module of tests for state"""
 import json
 import unittest
 from datetime import datetime
 from models.state import State
 
 
-class T_State(unittest.TestCase):
-    '''test ameity class'''
+class TestsState(unittest.TestCase):
+    """Class for tests State"""
 
-    def test_ty(self):
-        '''check types'''
-        clstest = Amenity()
-        self.assertEqual(type(clstest), State)
-        self.assertEqual(type(clstest.id), str)
-        self.assertEqual(type(clstest.created_at), datetime)
-        self.assertEqual(type(clstest.updated_at), datetime)
-        self.assertEqual(type(clstest.__str__()), str)
-        self.assertNotEqual(len(clstest.__str__()), 0)
-        self.assertEqual(type(clstest.name), str)
-        self.assertEqual(str(clstest), "[State] ({}) \
-        {}".format(clstest.id, clstest.__dict__))
+    def test_creation(self):
+        """Tests for state creation"""
+        new_s = State()
+        self.assertEqual(type(new_s), State)
 
-    def test_meth(self):
-        '''test method'''
-        cls_test = State()
-        self.assertEqual(type(cls_test.to_dict()), dict)
+        self.assertEqual(type(new_s.id), str)
+        self.assertEqual(type(new_s.created_at), datetime)
+        self.assertEqual(type(new_s.updated_at), datetime)
+        self.assertEqual(type(new_s.__str__()), str)
+        self.assertNotEqual(len(new_s.__str__()), 0)
+        self.assertEqual(type(new_s.name), str)
+        self.assertEqual(str(new_s), f'[State] ({new_s.id}) \
+{new_s.__dict__}')
+
+    def test_methods(self):
+        """Tests for user methods"""
+        new_s = State()
+        self.assertEqual(type(new_s.to_dict()), dict)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-'''test user'''
-
+"""Module of tests for user"""
 import json
 import unittest
 from datetime import datetime
 from models.user import User
 
 
-class T_User(unittest.TestCase):
-    '''test user class'''
+class TestsUser(unittest.TestCase):
+    """Class for tests user"""
+    
+    def test_creation(self):
+        """Tests for user creation"""
+        new_u = User()
+        self.assertEqual(type(new_u), User)
 
-    def test_ty(self):
-        '''check types'''
-        clstest = User()
-        self.assertEqual(type(clstest), User)
-        self.assertEqual(type(clstest.id), str)
-        self.assertEqual(type(clstest.created_at), datetime)
-        self.assertEqual(type(clstest.updated_at), datetime)
-        self.assertEqual(type(clstest.__str__()), str)
-        self.assertNotEqual(len(clstest.__str__()), 0)
-        self.assertEqual(type(clstest.name), str)
-        self.assertEqual(str(clstest), "[User] ({}) \
-        {}".format(clstest.id, clstest.__dict__))
+        self.assertEqual(type(new_u.id), str)
+        self.assertEqual(type(new_u.created_at), datetime)
+        self.assertEqual(type(new_u.updated_at), datetime)
+        self.assertEqual(type(new_u.__str__()), str)
+        self.assertNotEqual(len(new_u.__str__()), 0)
+        self.assertEqual(type(new_u.first_name), str)
+        self.assertEqual(type(new_u.last_name), str)
+        self.assertEqual(type(new_u.email), str)
+        self.assertEqual(type(new_u.password), str)
+        self.assertEqual(str(new_u), f'[User] ({new_u.id}) \
+{new_u.__dict__}')            
 
-    def test_meth(self):
-        '''test method'''
-        cls_test = User()
-        self.assertEqual(type(cls_test.to_dict()), dict)
+    def test_methods(self):
+        """Tests for user methods"""
+        new_u = User()
+        self.assertEqual(type(new_u.to_dict()), dict)
